1b2f6301a73f005ba62d48bf84aede3db0fa68a9
otter <otter@otter-VirtualBox>
2017-10-16 15:10:47 +0800

    minor fix

--- src/main/java/com/aeroplanechess/service/AbstractWebSocketService.java
+++ src/main/java/com/aeroplanechess/service/AbstractWebSocketService.java
@@ -7,9 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Service;
 
-@Service
 public class AbstractWebSocketService {
 
 	Logger logger = LoggerFactory.getLogger(AbstractWebSocketService.class);
