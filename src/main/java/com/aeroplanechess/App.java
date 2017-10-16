a43a1c45931c89e297c6a9321c74daa91a42971c
otter <otter@otter-VirtualBox>
2017-10-16 11:08:17 +0800

    rename main class

--- src/main/java/com/aeroplanechess/App.java
+++ src/main/java/com/aeroplanechess/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AeroplaneChess {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AeroplaneChess.class);
+		SpringApplication.run(App.class);
 	}
 
 }
