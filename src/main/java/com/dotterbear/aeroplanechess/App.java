f6363995e6933b79be46553615f02f7a7cddc239
dotterbear <dotter.bear@gmail.com>
2017-10-17 21:30:02 +0800

    move component scan

--- src/main/java/com/dotterbear/aeroplanechess/App.java
+++ src/main/java/com/dotterbear/aeroplanechess/App.java
@@ -2,8 +2,12 @@ package com.dotterbear.aeroplanechess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication(scanBasePackages = { "com.dotterbear" })
+import com.dotterbear.websocket.gameroom.config.WebSocketConfig;
+
+@SpringBootApplication()
+@EnableConfigurationProperties(WebSocketConfig.class)
 public class App {
 
 	public static void main(String[] args) {
