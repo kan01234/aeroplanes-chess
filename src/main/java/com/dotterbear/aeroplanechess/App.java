3bf532115167767d45cc123fe3b71b8241ef4f3a
otter <otter@otter-VirtualBox>
2017-10-17 12:12:36 +0800

    add scan base packages

--- src/main/java/com/dotterbear/aeroplanechess/App.java
+++ src/main/java/com/dotterbear/aeroplanechess/App.java
@@ -3,7 +3,7 @@ package com.dotterbear.aeroplanechess;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "com.dotterbear" })
 public class App {
 
 	public static void main(String[] args) {
