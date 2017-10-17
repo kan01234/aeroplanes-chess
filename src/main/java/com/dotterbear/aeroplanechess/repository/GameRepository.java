50d2a2fc01639f47de41a7d04515d7959f6ed109
otter <otter@otter-VirtualBox>
2017-10-17 11:02:58 +0800

    bug fix

--- src/main/java/com/dotterbear/aeroplanechess/repository/GameRepository.java
+++ src/main/java/com/dotterbear/aeroplanechess/repository/GameRepository.java
@@ -42,7 +42,7 @@ public class GameRepository<T extends Game> {
 	public T removePlayingGame(String gameId) {
 		T t = playingGameMap.remove(gameId);
 		if (t != null)
-			playerRepository.removePlayer(Stream.of(t.getPlayers()).map(p -> p.getSessionId()).collect(Collectors.toList()));
+			playerRepository.removePlayer(Stream.of(t.getPlayers()).filter(p -> p != null).map(p -> p.getSessionId()).collect(Collectors.toList()));
 		return t;
 	}
 
