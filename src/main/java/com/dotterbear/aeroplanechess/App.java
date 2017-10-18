2242e17cca1fa44d56b6885a57d68180bee43685
otter <otter@otter-VirtualBox>
2017-10-18 10:48:30 +0800

    update websocket-gameroom ver.

--- src/main/java/com/dotterbear/aeroplanechess/App.java
+++ src/main/java/com/dotterbear/aeroplanechess/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-import com.dotterbear.websocket.gameroom.config.WebSocketConfig;
+import com.dotterbear.websocket.gameroom.WebSocketGameRoom;
 
-@SpringBootApplication()
-@EnableConfigurationProperties(WebSocketConfig.class)
+@SpringBootApplication
+@EnableConfigurationProperties(WebSocketGameRoom.class)
 public class App {
 
 	public static void main(String[] args) {
